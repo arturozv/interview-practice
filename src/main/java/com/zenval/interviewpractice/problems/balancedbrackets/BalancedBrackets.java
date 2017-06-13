9cb81c619189c44cfedffa79d6b2e75862628830
arturozv <zzarturozz@gmail.com>
2017-06-13 16:36:03 -0700

    balanced brackets

---
+++ src/main/java/com/zenval/interviewpractice/problems/balancedbrackets/BalancedBrackets.java
@@ -0,0 +1,48 @@
+package com.zenval.interviewpractice.problems.balancedbrackets;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * Created by arturo on 13/06/17.
+ */
+public class BalancedBrackets {
+
+    private final Map<Character, Character> characterMap = new HashMap<>();
+
+    public BalancedBrackets() {
+        characterMap.put('}', '{');
+        characterMap.put(']', '[');
+        characterMap.put(')', '(');
+        characterMap.put('>', '<');
+    }
+
+    public boolean isBalanced(final String input) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            Character character = input.charAt(i);
+
+            if (isClosener(character)) {
+                if (!stack.isEmpty() && characterMap.get(character).equals(stack.peek())) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else if (isOpener(character)) {
+                stack.push(character);
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    boolean isClosener(Character character) {
+        return characterMap.containsKey(character);
+    }
+
+    boolean isOpener(Character character) {
+        return characterMap.containsValue(character);
+    }
+}
---
+++ src/test/java/com/zenval/interviewpractice/problems/balancedbrackets/BalancedBracketsTest.java
@@ -0,0 +1,44 @@
+package com.zenval.interviewpractice.problems.balancedbrackets;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BalancedBracketsTest {
+
+    private BalancedBrackets instance = new BalancedBrackets();
+
+    @Test
+    public void when_is_balanced() {
+        String input = "{a[b(c<def>g)h]i}";
+        assertThat(instance.isBalanced(input)).isTrue();
+
+        input = "{[(<>)]}";
+        assertThat(instance.isBalanced(input)).isTrue();
+
+        input = "[[[[[[[[[]]]]]]]]]";
+        assertThat(instance.isBalanced(input)).isTrue();
+    }
+
+
+    @Test
+    public void when_is_not_balanced() {
+        String input = "{ab(c<def>g)h]i}";
+        assertThat(instance.isBalanced(input)).isFalse();
+
+        input = "{a[b(c<defg)h]i}";
+        assertThat(instance.isBalanced(input)).isFalse();
+
+        input = "{a[b(c<def>g)h]i";
+        assertThat(instance.isBalanced(input)).isFalse();
+
+        input = "ab(c<def>g)h]i}";
+        assertThat(instance.isBalanced(input)).isFalse();
+
+        input = "{ab(c<def>>g)h]i}";
+        assertThat(instance.isBalanced(input)).isFalse();
+
+        input = "{{ab(c<def>g)h]i}";
+        assertThat(instance.isBalanced(input)).isFalse();
+    }
+}
